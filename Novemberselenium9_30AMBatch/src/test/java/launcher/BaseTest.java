4d48b1daa77f043c67e89de0d8963b81ebe60c11
ravilella1234 <masters.selenium@gmail.com>
2024-11-26 10:59:51 +0530

    weblocators day-2

--- Novemberselenium9_30AMBatch/src/test/java/launcher/BaseTest.java
+++ Novemberselenium9_30AMBatch/src/test/java/launcher/BaseTest.java
@@ -3,7 +3,9 @@ package launcher;
 import java.io.FileInputStream;
 import java.util.Properties;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -16,6 +18,7 @@ public class BaseTest
 	public static Properties p;
 	public static Properties mainProp;
 	public static Properties childProp;
+	public static Properties orProp;
 	
 	public static void init() throws Exception
 	{
@@ -36,6 +39,10 @@ public class BaseTest
 		childProp.load(fis);
 		String url = childProp.getProperty("amazonurl");
 		System.out.println(url);
+		
+		fis = new FileInputStream(projectResourcePath+"or.properties");
+		orProp = new Properties();
+		orProp.load(fis);
 	}
 	
 	public static void launch(String browser)
@@ -47,6 +54,7 @@ public class BaseTest
 		}else if (p.getProperty(browser).equals("edge")) {
 			driver = new EdgeDriver();
 		}
+		driver.manage().window().maximize();
 	}
 	
 	public static void navigateurl(String url)
@@ -54,5 +62,49 @@ public class BaseTest
 		//driver.get(url);
 		driver.navigate().to(childProp.getProperty(url));
 	}
+	
+	
+	public static void clickElement(String locatorKey) 
+	{
+		getElementType(locatorKey).click();
+		//driver.findElement(By.xpath(orProp.getProperty(locatorKey))).click();
+	}
+
+	public static void typeText(String locatorKey, String text) 
+	{
+		getElementType(locatorKey).sendKeys(text);
+		//driver.findElement(By.name(orProp.getProperty(locatorKey))).sendKeys(text);
+	}
+
+	public static void selectDropdownOption(String locatorKey, String option) 
+	{
+		getElementType(locatorKey).sendKeys(option);
+		//driver.findElement(By.id(orProp.getProperty(locatorKey))).sendKeys(option);
+	}
+	
+	public static WebElement getElementType(String locatorKey) 
+	{
+		WebElement element = null;
+		
+		if(locatorKey.endsWith("_id")) {
+			element = driver.findElement(By.id(orProp.getProperty(locatorKey)));
+		}else if (locatorKey.endsWith("_name")) {
+			element = driver.findElement(By.name(orProp.getProperty(locatorKey)));
+		}else if (locatorKey.endsWith("_classname")) {
+			element = driver.findElement(By.className(orProp.getProperty(locatorKey)));
+		}else if (locatorKey.endsWith("_xpath")) {
+			element = driver.findElement(By.xpath(orProp.getProperty(locatorKey)));
+		}else if (locatorKey.endsWith("_css")) {
+			element = driver.findElement(By.cssSelector(orProp.getProperty(locatorKey)));
+		}else if (locatorKey.endsWith("_linktext")) {
+			element = driver.findElement(By.linkText(orProp.getProperty(locatorKey)));
+		}else if (locatorKey.endsWith("_partiallinktext")) {
+			element = driver.findElement(By.partialLinkText(orProp.getProperty(locatorKey)));
+		}
+		
+		
+		return element;
+	}
+
 
 }
--- Novemberselenium9_30AMBatch/src/test/java/launcher/TC_002.java
+++ Novemberselenium9_30AMBatch/src/test/java/launcher/TC_002.java
@@ -1,5 +1,9 @@
 package launcher;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+
 public class TC_002 extends BaseTest
 {
 
@@ -11,9 +15,23 @@ public class TC_002 extends BaseTest
 		
 		navigateurl("amazonurl");
 		
-		driver.manage().window().maximize();
+		selectDropdownOption("amazonsearchdropbox_id","Books");
+		
+		typeText("amazonsearchtextbox_id","Harry Potter");
+		
+		clickElement("amazonsearchbutton_xpath");
 		
 		
+		//WebElement loc = driver.findElement(By.id("searchDropdownBox"));
+		//loc.sendKeys("Books");
+		
+		//driver.findElement(By.id("searchDropdownBox")).sendKeys("Books");
+		
+		//driver.findElement(By.name("field-keywords")).sendKeys("Harry Potter");
+		
+		//driver.findElement(By.xpath("//*[@id=\"nav-search-submit-button\"]")).click();
+		//driver.findElement(By.xpath("//*[@id=\"nav-search-submit-button\"]")).sendKeys(Keys.ENTER);
 	}
 
+	
 }
