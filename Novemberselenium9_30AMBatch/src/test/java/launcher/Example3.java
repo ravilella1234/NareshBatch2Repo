ff291ad2c617c187c0ba06e08469ae72def87d2a
ravilella1234 <masters.selenium@gmail.com>
2024-12-05 10:57:32 +0530

    Example day-3

---
+++ Novemberselenium9_30AMBatch/src/test/java/launcher/Example3.java
@@ -0,0 +1,44 @@
+package launcher;
+
+import java.security.Key;
+import java.time.Duration;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Example3 
+{
+
+	public static void main(String[] args) throws Exception 
+	{
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.google.com/");
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.findElement(By.id("APjFqb")).sendKeys("Hadoop");
+		//Thread.sleep(3000);
+		
+		//div[@jsname='erkvQe']//ul/li//descendant::div[@class='wM6W7d']/span
+		//div[@id='Alh6id']//ul/li/div//descendant::div[@class='wM6W7d']/span
+		
+		List<WebElement> listElements = driver.findElements(By.xpath("//div[@jsname='erkvQe']//ul/li//descendant::div[@class='wM6W7d']/span"));
+		
+		System.out.println("Elements size : " + listElements.size());
+	
+		for(WebElement element:listElements)
+		{
+			System.out.println(element.getText());
+			if(element.getText().contains("hadoop tutorial"))
+				element.click();
+			break;
+		}
+	}
+
+}
